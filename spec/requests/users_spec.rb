e5e35fc728bec002de3e5b5524a4d2e32e831dfc
Dean Marano <here@dean.is>
2023-01-15 13:03:18 -0500

    Add factory bot, fix spec for logged in.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "requires authorization" do
       get users_path
+      expect(response).to have_http_status(401)
+    end
+
+    let(:user)        { FactoryBot.create(:user) }
+    let(:token)       { FactoryBot.create(:access_token,
+                                          resource_owner_id: user.id) }
+
+    it "returns users" do
+      get users_path, params: {}, headers: { 'Authorization': 'Bearer ' + token.token }
       expect(response).to have_http_status(200)
+      body = JSON.parse(response.body)
+      expect(body.length).to eq(1)
     end
   end
 end
